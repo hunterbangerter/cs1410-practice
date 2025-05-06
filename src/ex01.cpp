bd0eb6fd1eab305e0d5fe434631970e390d8cf5a
hunterbangerter <105174434+hunterbangerter@users.noreply.github.com>
2025-05-06 18:23:56 +0000

    finished ex01

---
+++ src/ex01.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+
+using namespace std;
+
+int main(){
+    int x,y;
+    cout << "Enter X: ";
+    cin >> x;
+    cout << "\nEnter Y: ";
+    cin >> y;
+    cout << "\n X + Y = " << (x + y) << endl;
+    cout << "\n X - Y = " << (x - y) << endl;
+    cout << "\n X * Y = " << (x * y) << endl;
+    cout << "\n X / Y = " << (x / y) << endl;
+
+  return 0;
+}

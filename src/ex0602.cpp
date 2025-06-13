1e07e15b76fd32941e7fcac223485efcb6be9702
hunterbangerter <105174434+hunterbangerter@users.noreply.github.com>
2025-06-13 18:28:59 +0000

    finished ex0602

--- src/ex0602.cpp
+++ src/ex0602.cpp
@@ -2,10 +2,20 @@
 # include <string>
 # include <cmath>
 # include <iostream>
-
 using namespace std;
 
 int main(){
+    int bills[] {10, 20, 50};
+    int* p = bills;
 
-  return 0;
+    cout << *p << ", " << *(p+1) << ", " << *(p+2) << endl;
+    bills[2] = 100;
+    *(p+2) = 100;
+
+    int* dbills = new int[3]{10, 20, 50};
+    cout << *dbills << ", " << *(dbills+1) << ", " << *(dbills+2) << endl;
+    cout << dbills[0] << ", " << dbills[1] << ", " << dbills[2] << endl;
+    delete[] dbills;
+
+    return 0;
 }

980b6b3750cf783ac2f82c090481c80706788d6e
hunterbangerter <105174434+hunterbangerter@users.noreply.github.com>
2025-06-13 18:35:55 +0000

    finished ex0603

--- src/ex0603.cpp
+++ src/ex0603.cpp
@@ -5,7 +5,55 @@
 
 using namespace std;
 
+enum class BillKind {
+    one, five, ten, twenty
+
+};
+struct cashinonebillkind{
+ BillKind kind;
+ int count;
+
+};
+struct cash {
+    cashinonebillkind ones;
+    cashinonebillkind fives;
+    cashinonebillkind tens;
+    cashinonebillkind twenties;
+
+};
+int totalcash(cash& c){
+    return(c.ones.count * 1 +
+    c.fives.count * 5 +
+    c.tens.count * 10 +
+    c.twenties.count * 20);
+}
+int totalcash2(cash* c){
+    return(
+    c->ones.count * 1 +
+    c->fives.count * 5 +
+    c->tens.count * 10 +
+    c->twenties.count * 20);
+}
 int main(){
+
+    cash inwallet {
+        {BillKind::one, 2},
+        {BillKind::five, 3},
+        {BillKind::ten, 1},
+        {BillKind::twenty, 3},
+    };
+
+    cout << "Total cash in wallet: " << totalcash(inwallet) << endl;
+    cout << "Total cash in wallet2: " << totalcash2(&inwallet) << endl;
+
+    cash *dwallet = new cash{
+        {BillKind::one, 2},
+        {BillKind::five, 3},
+        {BillKind::ten, 1},
+        {BillKind::twenty, 3},
+    };
+    cout << "Total cash in wallet: " << totalcash(*dwallet) << endl;
+    cout << "Total cash in wallet2: " << totalcash2(dwallet) << endl;
 
   return 0;
 }

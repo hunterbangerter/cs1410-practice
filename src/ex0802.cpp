16d7adaf657e36ba2690b8a6d940e98d3ea5829a
hunterbangerter <105174434+hunterbangerter@users.noreply.github.com>
2025-07-07 21:20:54 +0000

    finished ex0802

--- src/ex0802.cpp
+++ src/ex0802.cpp
@@ -1,10 +1,39 @@
-# include <iomanip>
 # include <string>
-# include <cmath>
 # include <iostream>
+#include "author.h"
 using namespace std;
 
+
+class Book {
+    private:
+        string title;
+        int year;
+        Author author;
+    public:
+        Book(const string& t, int y, const Author& a):title(t), year(y), author(a){}
+
+        string getTitle() const {return title;}
+        int getYear() const {return year;}
+        string getAuthor() const {return title;}
+
+        void setTitle(const string& t){title = t;}
+        void setYear(int y){year = y;}
+        void setAuthor(const Author& a) {author = a;}
+
+        void show() const{
+            cout << "book: " << title << "(" << year << " )\n"
+            << " Author: " << author.getName() << "[" << author.getNationality() << "]\n";
+
+
+        }
+};
+
 int main(){
+    Author a("george orwell", "british");
+    Book b("1984", 1949, a);
+
+    b.show();
+
 
   return 0;
 }
